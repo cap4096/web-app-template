3d66ca2901f2c72e6050cc539c29e8924544d2e9
Mats Loman <mats.loman@rte.se>
2017-09-24 16:26:54 +0200

    ESLint + Log Framework added.

--- path_config.js
+++ path_config.js
@@ -1,12 +1,12 @@
 const path = require('path');
 const outDir='dist';
 
-const DEVELOPMENT = process.env.NODE_ENV === "development";
-const PRODUCTION = process.env.NODE_ENV === "production";
+//const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
 module.exports = {
-    outDir : outDir,
-    output: path.resolve(__dirname, outDir),
-    publicPath: '/', //PRODUCTION ? '/' : `/${outDir}/`,
-    outFilename:  PRODUCTION ? '[hash]-bundle.min.js' : 'bundle.js'
+	outDir : outDir,
+	output: path.resolve(__dirname, outDir),
+	publicPath: '/', //PRODUCTION ? '/' : `/${outDir}/`,
+	outFilename:  PRODUCTION ? '[hash]-bundle.min.js' : 'bundle.js'
 };
--- server.js
+++ server.js
@@ -1,47 +1,68 @@
-"use strict";
+'use strict';
 
-const path = require('path');
+//
+// Initialize logging framework.
+//
+var winston = require('winston');
+var logger = new(winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({
+			level: 'debug'
+		}),
+
+		new (winston.transports.File)({
+			filename: 'log-file.log',
+			level: 'debug'
+		})
+	]
+});
+
+//const path = require('path');
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
+//const io = require('socket.io')(server);
 const pathConfig = require('./path_config');
 
 const port = process.env.PORT || 3000;
-const DEVELOPMENT = process.env.NODE_ENV === "development";
-const PRODUCTION = process.env.NODE_ENV === "production";
+const DEVELOPMENT = process.env.NODE_ENV === 'development';
+const PRODUCTION = process.env.NODE_ENV === 'production';
 
 if(PRODUCTION){
+	logger.info('Running server in production mode.');
+} else {
+	logger.info('Running server in development mode.');
 }
+
 app.use('/', express.static(pathConfig.outDir));
 
 if(DEVELOPMENT){
-    const webpack = require('webpack');
-    const config = require('./webpack.config.js');
-    const WebpackDevServer = require('webpack-dev-server');
+	const webpack = require('webpack');
+	const config = require('./webpack.config.js');
+	//const WebpackDevServer = require('webpack-dev-server');
 
-    config.entry.client.unshift(
-	`webpack-hot-middleware/client?http://localhost:${port}/`);
-    config.devServer.contentBase = pathConfig.output;
-    config.devServer.inline = true;
-    config.devServer.hot = true;
-    config.plugins.push(new webpack.HotModuleReplacementPlugin());
-    config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
+	config.entry.client.unshift(
+		`webpack-hot-middleware/client?http://localhost:${port}/`);
+	config.devServer.contentBase = pathConfig.output;
+	config.devServer.inline = true;
+	config.devServer.hot = true;
+	config.plugins.push(new webpack.HotModuleReplacementPlugin());
+	config.plugins.push(new webpack.NoEmitOnErrorsPlugin());
 
-    config.devtool = 'source-map';
+	config.devtool = 'source-map';
 
-    const webpackDevMiddleware = require('webpack-dev-middleware');
-    const compiler = webpack(config);
+	const webpackDevMiddleware = require('webpack-dev-middleware');
+	const compiler = webpack(config);
 
-    app.use(webpackDevMiddleware(compiler, {
-	filename: config.output.filenme,
-	publicPath: config.output.publicPath,
-	stats: {
-	    colors: true
-	}
-    }));
+	app.use(webpackDevMiddleware(compiler, {
+		filename: config.output.filenme,
+		publicPath: config.output.publicPath,
+		stats: {
+			colors: true
+		}
+	}));
 
-    app.use(require('webpack-hot-middleware')(compiler));
+	app.use(require('webpack-hot-middleware')(compiler));
 }
 
 server.listen(port);
